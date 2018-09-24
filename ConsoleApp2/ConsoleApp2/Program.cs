0357f62226b271a6886d8011c4c4d6600945f658
Ryan <werner.ryan18@gmail.com>
2018-09-24 12:34:46 -0700

    Added Files

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting System");
+            Console.WriteLine("Remember, we're 'accounting' on you!");
+            Console.ReadLine();
+        }
+    }
+}
